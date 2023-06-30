2b8a544e9966951312a65d90b1ffe0d96b863ee9
Bencolie <126235265+Bencolie@users.noreply.github.com>
2023-06-30 20:59:42 +0800

    Delete models.py

--- models.py
+++
@@ -1,7 +0,0 @@
-from website import db
-
-class Tracks(db.Model):
-    __tablename__ = 'MTracks'
-    id = db.Column(db.Integer,primary_key=True)
-    name = db.Column(db.String(30))
-    dlink = db.Column(db.String(8000))
